0392d7570e5835f6738b4aa9a01cbb5d12b9e210
Ey0s <eyosyasyosef1934@gmail.com>
2026-03-12 20:21:17 +0300

    add passwordAnalyzer.js

---
+++ src/passwordAnalyzer.js
@@ -0,0 +1,59 @@
+const { calculateEntropy } = require("./entropy");
+const { detectPatterns } = require("./patterns");
+const { estimateCrackTime } = require("./crackTime");
+
+function analyzePassword(password) {
+
+    let score = 0;
+    let feedback = [];
+
+    const checks = {
+        length: password.length >= 12,
+        uppercase: /[A-Z]/.test(password),
+        lowercase: /[a-z]/.test(password),
+        number: /[0-9]/.test(password),
+        special: /[^A-Za-z0-9]/.test(password),
+        repeated: /(.)\1{2,}/.test(password)
+    };
+
+    if (checks.length) score += 25;
+    else feedback.push("Password should be at least 12 characters");
+
+    if (checks.uppercase) score += 10;
+    else feedback.push("Add uppercase letters");
+
+    if (checks.lowercase) score += 10;
+    else feedback.push("Add lowercase letters");
+
+    if (checks.number) score += 10;
+    else feedback.push("Add numbers");
+
+    if (checks.special) score += 15;
+    else feedback.push("Add special characters");
+
+    if (!checks.repeated) score += 10;
+    else feedback.push("Avoid repeated characters");
+
+    const patternResults = detectPatterns(password);
+
+    if (patternResults.sequential)
+        feedback.push("Avoid sequential patterns (abc, 123)");
+
+    if (patternResults.keyboard)
+        feedback.push("Avoid keyboard patterns (qwerty)");
+
+    const entropy = calculateEntropy(password);
+    score += Math.min(20, Math.floor(entropy / 4));
+
+    const crackTime = estimateCrackTime(entropy);
+
+    return {
+        password,
+        score,
+        entropy,
+        crackTime,
+        feedback
+    };
+}
+
+module.exports = { analyzePassword };
